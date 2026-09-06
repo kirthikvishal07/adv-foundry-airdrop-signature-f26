ea61e168b7addaa9e1dd9fe16af2b2222c3f8baf
kirthikvishal07 <kirthikvishal07@gmail.com>
2026-09-06 13:59:17 +0000

    before signature ver

---
+++ script/DeployMerkleAirdrop.s.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {MerkleAirdrop, IERC20} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+
+contract DeployMerkleAirdrop is Script {
+    bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 private s_amountToTransfer = 4 * 25 * 1e18;
+
+
+    function deployMerkleAirdrop() external returns (MerkleAirdrop, BagelToken) {
+        vm.startBroadcast();
+        BagelToken token = new BagelToken();
+        MerkleAirdrop airdrop = new MerkleAirdrop(s_merkleRoot,IERC20(address(token)));
+
+        token.mint(token.owner(),s_amountToTransfer);
+        token.transfer(address(airdrop),s_amountToTransfer);
+        vm.stopBroadcast();
+        return (airdrop, token);
+    }
+}
+       // token.approve(address(airdrop),s_amountToTransfer);
